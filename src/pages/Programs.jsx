e1acc4e858d0409f475814e499d877d7cf2206f9
Udeme01 <emmanueluddy2@gmail.com>
2025-06-13 08:43:01 +0100

    fix: changed text size

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -6,7 +6,7 @@ const Programs = () => {
       <h1 className="text-4xl leading-12 font-medium animate-bounce">
         Coming Soon!
       </h1>
-      <p>We're working hard to finish the development of this feature.</p>
+      <p className="text-sm">We're working hard to finish the development of this feature.</p>
       <button className="bg-slate-700 text-white px-4 py-3 mt-6 tracking-wider rounded transition duration-500 hover:scale-95 hover:bg-slate-700/95 cursor-pointer">
         <Link to="/">Go Back Home</Link>
       </button>
