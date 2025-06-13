4e0aae97d41bd0d62da2e895779d7ced389dfa31
Udeme01 <emmanueluddy2@gmail.com>
2025-06-13 08:30:52 +0100

    fix: styles

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router";
 
 const Programs = () => {
   return (
-    <section className="h-[90vh] pt-40 flex flex-col items-center justify-center font-montserrat text-justify mx-6">
-      <h1 className="text-4xl leading-24 font-medium animate-bounce">
+    <section className="h-[90vh] pt-40 flex flex-col items-center justify-center font-montserrat text-center mx-6">
+      <h1 className="text-4xl leading-12 font-medium animate-bounce">
         Coming Soon!
       </h1>
       <p>We're working hard to finish the development of this feature.</p>
-      <button className="bg-slate-700 text-white px-4 py-3 tracking-wider rounded transition duration-500 hover:scale-95 hover:bg-slate-700/95 cursor-pointer">
+      <button className="bg-slate-700 text-white px-4 py-3 mt-6 tracking-wider rounded transition duration-500 hover:scale-95 hover:bg-slate-700/95 cursor-pointer">
         <Link to="/">Go Back Home</Link>
       </button>
     </section>
